4a26aa9b6b9afa09e9e84623e9a8112a04fe41b9
Sam Minton <sam@purecreative.digital>
2026-02-16 13:16:06 +1100

    Update setup.py to support Wagtail 7 and Python 3.14

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     description='A Django blog app implemented in Wagtail.',
     long_description=codecs.open(os.path.join(os.path.dirname(__file__), 'README.rst'), encoding='utf-8').read(),
     install_requires=[
-        'wagtail>=5.2,<7.0',
+        'wagtail>=5.2,<7.1',
         'django-el-pagination==4.0.0',
         'django-taggit>=5.0,<6.2',
         'wagtail-markdown==0.11.1'
@@ -42,14 +42,14 @@ setup(
         'Framework :: Wagtail',
         'Framework :: Wagtail :: 5',
         'Framework :: Wagtail :: 6',
+        'Framework :: Wagtail :: 7',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
         'Operating System :: OS Independent',
         'Topic :: Software Development'
     ]
